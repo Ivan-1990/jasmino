e4dcbcd36c0bbf5a9cd3c100cb1ecc5623383ec3
Evgeny Gorborukov <evgeny.gorborukov@gmail>
2012-11-23 13:42:58 +0300

    add to cart mechanism done

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,22 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+  
+  def show
+  	@product = Product.find(params[:id])
+  end
+  
+  def new
+  	@product = Product.new
+  	respond_to do |format|
+  	  format.html # new.html.erb
+  	  format.json { render json: @post }
+  	end
+  end
+  
+  def create
+  	@product = Product.create(params[:product])
+  	redirect_to product_path(@product)
+  end
+end
